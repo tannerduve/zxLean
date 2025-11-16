34c6d6617bd8ee9a5668f071734f846827856e12
Tanner Duve <122851174+tannerduve@users.noreply.github.com>
2025-11-16 01:31:20 -0800

    Euler decomp (#11)
    * workflow
    
    * euler_decomp
    
    * workflows
    
    * fix tests

--- tests/XFusion.lean
+++ tests/XFusion.lean
@@ -3,24 +3,24 @@ open ZxTerm Real
 
 /-- X spider fusion adds phases -/
 theorem x_fusion_basic :
-  ZxEquiv (X (1/4) 1 1 ; X (1/4) 1 1) (X (1/2) 1 1) := by
-  have h := ZxEquiv.x_fus (1/4) (1/4) (n := 1) (m := 1) (k := 1)
+  ZxEquiv (X 1 1 1 ; X 1 1 1) (X 2 1 1) := by
+  have h := ZxEquiv.x_fus 1 1 (n := 1) (m := 1) (k := 1)
   apply ZxEquiv.trans h
   ring_nf
   exact ZxEquiv.refl _
 
 /-- X fusion with zero phase -/
 theorem x_fusion_with_zero :
-  ZxEquiv (X 0 1 1 ; X (1/3) 1 1) (X (1/3) 1 1) := by
-  have h := ZxEquiv.x_fus 0 (1/3) (n := 1) (m := 1) (k := 1)
+  ZxEquiv (X 0 1 1 ; X 2 1 1) (X 2 1 1) := by
+  have h := ZxEquiv.x_fus 0 2 (n := 1) (m := 1) (k := 1)
   apply ZxEquiv.trans h
   ring_nf
   exact ZxEquiv.refl _
 
 /-- X fusion to full rotation -/
 theorem x_fusion_full_rotation :
-  ZxEquiv (X (1/2) 1 1 ; X (3*1/2) 1 1) (X (2*1) 1 1) := by
-  have h := ZxEquiv.x_fus (1/2) (3*1/2) (n := 1) (m := 1) (k := 1)
+  ZxEquiv (X 2 1 1 ; X 6 1 1) (X 8 1 1) := by
+  have h := ZxEquiv.x_fus 2 6 (n := 1) (m := 1) (k := 1)
   apply ZxEquiv.trans h
   ring_nf
   exact ZxEquiv.refl _
